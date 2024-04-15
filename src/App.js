c9f51d03707b33a04a3ceb0578c656a5ca4f0e0c
Bek <chndn8486@gmail.com>
2024-04-15 16:17:34 +0530

    sc

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import React, { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
 const App = () => {
   const pageSize = 5;
+  // const apiKey = process.env.REACT_APP_NEWS_API
   const apiKey = process.env.REACT_APP_NEWS_API
   const [progress, setProgress] = useState(0)
 
